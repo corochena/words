a478fbd6049e1b7d3ce93cbe78a9911380643c34
Carlos Peña <corochena@gmail.com>
2019-01-15 17:28:21 -0600

    Plotting by language

--- main.py
+++ main.py
@@ -89,4 +89,16 @@ plt.plot(stats.length, stats.unique, "bo")
 plt.loglog(stats.length, stats.unique, "bo")
 
 #lets stratify by language
-stats[stats.language == "English"]
+plt.figure(figsize = (10,10))
+subset = stats[stats.language == "English"]
+plt.loglog(subset.length, subset.unique, "o", label="English", color="crimson")
+subset = stats[stats.language == "French"]
+plt.loglog(subset.length, subset.unique, "o", label="French", color="forestgreen")
+subset = stats[stats.language == "German"]
+plt.loglog(subset.length, subset.unique, "o", label="German", color="orange")
+subset = stats[stats.language == "Portuguese"]
+plt.loglog(subset.length, subset.unique, "o", label="Portuguese", color="blueviolet")
+plt.legend()
+plt.xlabel("Book length")
+plt.ylabel("Number of Unique words")
+plt.savefig("lang_plot.pdf")
